a37a6a7052663016e0f24f8fc988c2e8942a50f8
Yog Sharma <yogthesharma@gmail.com>
2021-01-06 11:37:30 +0530

    First Bare Minimum Master Commit

---
+++ pages/index.js
@@ -0,0 +1,15 @@
+import Head from "next/head";
+import styles from "../styles/Home.module.css";
+
+export default function Home() {
+  return (
+    <div className={styles.container}>
+      <Head>
+        <title>Create Next App</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <h1>Testing of git images goes here.</h1>
+    </div>
+  );
+}
