6b57c5e4984dfc5463e54f3335239e702a5775aa
Yog Sharma <yogthesharma@gmail.com>
2021-01-06 12:56:53 +0530

    Added Image Functionalities and also added a new image

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,6 @@
 import Head from "next/head";
 import styles from "../styles/Home.module.css";
+import Image from "next/image";
 
 export default function Home() {
   return (
@@ -10,6 +11,24 @@ export default function Home() {
       </Head>
 
       <h1>Testing of git images goes here.</h1>
+      <div
+        style={{
+          width: "500px",
+          height: "500px",
+          backgroundColor: "orange",
+          position: "relative",
+          borderRadius: "20px",
+          overflow: "hidden",
+        }}
+      >
+        <Image
+          src="/image.jpg"
+          layout="fill"
+          // loading="lazy"
+          alt="Picture of scene"
+          priority={true}
+        />
+      </div>
     </div>
   );
 }
